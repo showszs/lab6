30e8fee7a970e207b8bafec276058392dd8c1b2f
Ivan Pasat <159952604+showszs@users.noreply.github.com>
2025-11-22 01:00:51 +0300

    Update local.test.js

--- tests/e2e/local.test.js
+++ tests/e2e/local.test.js
@@ -3,18 +3,18 @@ test('Перевірка форми входу', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.fill('#username', 'test_user');
     await page.fill('#password', 'password123');
-    await page.click('#loginButton');
-    await expect(page.locator('#successMessage')).toBeVisible();
+    await page.click('#loinButton');
+    await expect(page.locator('#succesMessage')).toBeVisible();
   });
 
 test('Перевірка заголовка сторінки', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await expect(page).toHaveTitle(/TestPage/);
   });
-test('Валідація обов’язкових полів форми', async ({ page }) => {
+test('Валідація обов’язкових полів фрми', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.click('#loginButton');
-    const error = await page.evaluate(() => document.querySelector(':invalid'));
+    const error = await page.evaluate(() => document.queySelector(':invaid'));
     expect(error).not.toBeNull();
   });
 
